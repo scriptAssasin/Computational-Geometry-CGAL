bc377265f66e0b24b2167726753284bfcb7d9bce
Aggelos Fakorellis <afakorellis@evolutionprojects.gr>
2022-12-05 22:36:31 +0200

    WIP aka work in progress 85%

--- src/localSearch.cpp
+++ src/localSearch.cpp
@@ -23,9 +23,65 @@ bool is_visible_path(Polygon_2 &polygon, Points path, Segment_2 edge)
     return true;
 }
 
-Polygon_2 move(Polygon_2 &starting_polygon, Points polygon_line, Segment_2 edge)
+Polygon_2 move(Polygon_2 &starting_polygon, Points &polygon_line, Segment_2 edge)
 {
-    Polygon_2 final_polygon;
+
+    Polygon_2 final_polygon(starting_polygon), temp;
+
+    // cout << "----------" << endl;
+    // printtVector(polygon_line);
+    // cout << "----------" << endl;
+    // for (vertexit vertit = final_polygon.vertices_begin(); vertit != final_polygon.vertices_end(); ++vertit)
+    // {
+    //     cout << *vertit << endl;
+    // }
+    // cout << "-------------------" << endl;
+
+    vertexit starting_point_iterator, ending_point_iterator;
+
+    for (vertexit vertit = final_polygon.vertices_begin(); vertit != final_polygon.vertices_end(); ++vertit)
+    {
+        if (*vertit == polygon_line[0])
+        {
+            starting_point_iterator = vertit;
+        }
+        if (*vertit == polygon_line[polygon_line.size() - 1])
+        {
+            ending_point_iterator = vertit;
+        }
+    }
+
+    cout << *starting_point_iterator << " - " << *ending_point_iterator << endl;
+
+    if ((starting_point_iterator == final_polygon.vertices_end() - 1))
+    {
+    }
+    else if (starting_point_iterator == final_polygon.vertices_end() - 2)
+    {
+    }
+    else
+    {
+        final_polygon.erase(starting_point_iterator, ending_point_iterator);
+        vertexit edge_index;
+
+        for (vertexit vertit = final_polygon.vertices_begin(); vertit != final_polygon.vertices_end(); ++vertit)
+        {
+            if (*vertit == polygon_line[0])
+            {
+                starting_point_iterator = vertit;
+            }
+            if (*vertit == polygon_line[polygon_line.size() - 1])
+            {
+                ending_point_iterator = vertit;
+            }
+            if (*vertit == edge[0])
+            {
+                edge_index = vertit;
+            }
+        }
+
+        final_polygon.insert(edge_index + 1, starting_point_iterator, ending_point_iterator);
+    }
 
     return final_polygon;
 }
@@ -97,7 +153,7 @@ void localSearch(Points points, string parameter, double threshold)
 
     /** TODO: TO DELETE ENDS HERE **/
 
-    Polygon_2 initial_state_random_order = initial_state;
+    Polygon_2 initial_state_random_order(initial_state);
 
     std::random_shuffle(initial_state_random_order.begin(), initial_state_random_order.end());
 
@@ -106,13 +162,13 @@ void localSearch(Points points, string parameter, double threshold)
     double max_area = FLT_MIN, current_area, diff = FLT_MAX;
     while (diff >= threshold)
     {
-        Polygon_2 current_polygon = initial_state; // TODO: integrate with swap
+        Polygon_2 current_polygon(initial_state); // TODO: integrate with swap
 
         for (edgeit it = initial_state_random_order.edges_begin(); it != initial_state_random_order.edges_end(); ++it)
         {
             // std::cout << ' ' << *it << endl;
 
-            vector<tuple<Segment_2, Points>> swaps;
+            // vector<tuple<Segment_2, Points>> swaps;
             vector<Points> polygon_lines = calculate_polygon_lines(current_polygon);
 
             // for (vertexit vertit = initial_state.vertices_begin(); vertit != initial_state.vertices_end(); ++vertit)
@@ -124,23 +180,24 @@ void localSearch(Points points, string parameter, double threshold)
             {
                 Polygon_2 temp_polygon;
                 Points polygon_line = polygon_lines[i];
-                if (is_visible_path(current_polygon, polygon_line, *it))
-                {
-                    temp_polygon = move(current_polygon, polygon_line, *it);
 
-                    if (temp_polygon.area() > max_area)
+                cout << "BEFORE ENTERING IS VISIBLE PATH" << endl;
+
+                temp_polygon = move(current_polygon, polygon_line, *it);
+
+                if (temp_polygon.area() > max_area)
+                {
+                    if (!temp_polygon.is_simple())
                     {
-                        if(!temp_polygon.is_simple())
-                        {
-                            cout << "BACKTRACKING TO OPTIMIZE AREA!" << endl;
-                        }
-                        current_polygon = temp_polygon;
+                        cout << "OLD AREA: " << current_polygon.area() << " - NEW AREA: " << temp_polygon.area() << endl;
+                        //     cout << "BACKTRACKING TO OPTIMIZE AREA!" << endl;
                     }
+                    // current_polygon = temp_polygon;
                 }
             }
         }
 
         cout << "Is Simple??? " << current_polygon.is_simple() << endl;
-        break;
+        
     }
 }
